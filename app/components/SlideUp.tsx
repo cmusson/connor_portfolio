17e42b668ffd3888c084a8692872dec8db8b4569
Connor <mussonconnor@gmail.com>
2023-08-02 19:00:57 +0100

    added comments explaining SlideUp component

--- app/components/SlideUp.tsx
+++ app/components/SlideUp.tsx
@@ -10,11 +10,14 @@ export default function SlideUp({ children, offset = "0px" }: IUseClientProps) {
   const ref = useRef(null);
 
   useEffect(() => {
+    // intersection observer created with callback for intersection of viewport and ref
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
+            // remove classname of opacity-0 so it is visible
             entry.target.classList.remove("opacity-0");
+            // run the animation
             entry.target.classList.add("animate-slideUpCubiBezier");
           }
         });
@@ -22,6 +25,7 @@ export default function SlideUp({ children, offset = "0px" }: IUseClientProps) {
       { rootMargin: offset }
     );
 
+    // and observes ref firing the callback within when it intersects the viewport
     if (ref.current) {
       observer.observe(ref.current);
     }
