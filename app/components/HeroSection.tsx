9f020d902fce860154f91a1d07d7ba0d38f067b2
Connor <mussonconnor@gmail.com>
2024-06-03 10:20:13 +0100

    removed circle

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -28,15 +28,6 @@ const HeroSection = () => {
             the world around us.
           </p>
           <div className="flex items-center gap-2">
-            <div className="flex space-x-4">
-              <div className="rounded-full bg-blue-400 h-12 w-12"></div>
-              <div className="flex-1 space-y-4 py-1">
-                <div className="space-y-2">
-                  <div className="h-4 bg-blue-400 rounded"></div>
-                  <div className="h-4 bg-blue-400 rounded w-5/6"></div>
-                </div>
-              </div>
-            </div>
             <Link
               href={"#projects"}
               className=" text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
