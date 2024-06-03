339375eec16dc5dfdc2c652148d6c85ba84a7960
Connor <mussonconnor@gmail.com>
2024-06-03 10:13:23 +0100

    removed ping

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
             the world around us.
           </p>
           <div className="flex items-center gap-2">
-            <div className="animate-ping flex space-x-4">
+            <div className="flex space-x-4">
               <div className="rounded-full bg-blue-400 h-12 w-12"></div>
               <div className="flex-1 space-y-4 py-1">
                 <div className="h-4 bg-blue-400 rounded w-3/4"></div>
